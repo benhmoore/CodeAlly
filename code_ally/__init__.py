611e786da702dfb2b5db8f6b9e7f77b54bdf7df5
Ben Moore <ben@benhmoore.com>
2025-04-05 12:29:34 -0500

    Update version information.

--- code_ally/__init__.py
+++ code_ally/__init__.py
@@ -4,9 +4,9 @@ This package provides a set of tools for using local LLMs to assist with
 coding tasks through natural language, while keeping all data on your machine.
 """
 
-__version__ = "0.1.0"
-__author__ = "Code Ally Team"
-__email__ = "info@example.com"
+__version__ = "0.3.0"
+__author__ = "Ben H Moore"
+__email__ = "ben@benhmoore.com"
 
 # Import common configuration functions for easier access
 from code_ally.config import load_config, reset_config, save_config
